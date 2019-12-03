c5038c763406ba502e91c1c29dff69308c8ebd9b
Liam Keery <Liam@Liams-MacBook-Pro-2.local>
2019-12-03 15:31:40 -0500

    Added NodeMailer

--- public/js/main.js
+++ public/js/main.js
@@ -1,3 +1,54 @@
 (() => {
     console.log('Connected!')
+
+    const form = document.querySelector('#contact-form'), submit = form.querySelector('.submit');
+
+    console.log(form);
+
+    function handleMail(event) {
+        event.preventDefault();
+
+        // formdata will be the values of the fields the user fills out (the inputs)
+        // maildata is an object we'll build and send through with those values
+
+        let formdata = new FormData(form),
+            maildata = {};
+
+        // parse the form data (it's an iterable, so you have to do it this way)
+        // and populate the maildata object with the input values (the formdata entries)
+        for (let [key, value] of formdata.entries()) {
+            maildata[key] = value;
+        }
+
+        let url = `/mail`;
+
+        // use the POST superglobal which is more secure than GET, and hit the /mail route in index.js
+        // inside the routes folder. this will take in the formdata we're sending, and use that to send our email
+        fetch(url, {
+            method: 'POST',
+            headers: {
+                'Accept': 'application/json, text/plain, */*',
+                'Content-type': 'application/json'
+            },
+
+            body: JSON.stringify(maildata)
+        })
+            .then(res => res.json())
+            .then(data => {
+                // remove this when testing is done and everything is working
+                console.log(data);
+
+                if (data.response.includes("OK")) {
+                    // we successfully sent an email via gmail and nodemailer!
+                    // flash success here, reset the form
+                    form.reset();
+                    alert("email was sent!"); // DO NOT use alerts. they are so hacky and gross.
+                }
+            }) // this will be a success or fail message from the server
+            .catch((err) => console.log(err));
+
+        console.log('tried sending mail');
+    }
+
+    form.addEventListener('submit', handleMail)
 })();
--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,18 @@
 var express = require('express');
 var router = express.Router();
 
+const mailer = require('nodemailer');
+require('dotenv').config();
+var auth = require('../config/mailcreds');
 const sql = require('../utils/sql');
+
+const transporter = mailer.createTransport({
+	service: 'gmail',
+	auth: {
+		user: auth.user,
+		pass: auth.pass
+	}
+});
 
 router.get('/', function(req, res, next) {
   
@@ -25,5 +36,30 @@ router.get('/project/:target', (req, res) => {
     res.json(result[0]);
   })
 });
+
+router.post('/mail', (req, res) => {
+	console.log('hit mail route');
+	console.log('body: ', req.body);
+
+	// get the mail options from the form -> the url params using bodyParser middleware
+
+	const mailOptions = {
+		from: req.body.usermail,
+		to: auth.user,
+		replyTo: req.body.usermail,
+		subject: `CONTACT REQUEST FROM PORTFOLIO: ${req.body.name || 'none'}`, // Subject line
+		text: req.body.message
+	};
+
+	transporter.sendMail(mailOptions, function (err, info) {
+		if (err) {
+			console.log("failed... ", err);
+			res.json(err);
+		} else {
+			console.log("success! ", info);
+			res.json(info);
+		}
+	});
+})
 
 module.exports = router;
