6a1757b4c12107f3b8b229bfce476cc6d5fd8862
Liam Keery <l_keery@fanshaweonline.ca>
2019-12-07 17:59:23 -0500

    SQL update

--- utils/sql.js
+++ utils/sql.js
@@ -1,7 +1,8 @@
 const config = require("../config");
 const sql = require('mysql');
 
-const connect = sql.createConnection({
+const connect = sql.createPool({ // sql.createConnection
+    connectionLimit: 5,
     host: config.host,
     port: config.port,
     user: config.uname,
