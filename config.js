f40a6e6ec569d39ac4e73c3fc75dd9029bc80fbd
Liam Keery <l_keery@fanshaweonline.ca>
2019-12-07 18:14:23 -0500

    Final update

--- config.js
+++ config.js
@@ -1,8 +1,8 @@
 const config = {
     host: "us-cdbr-iron-east-05.cleardb.net", //localhost
     uname: "bcdd7d72933758", // root
-    pword: "heroku_96ca2404358326c", // root
-    database: "db_portfolio"
+    pword: "03a6a1cc", // root
+    database: "heroku_96ca2404358326c"
 }
 
 module.exports = config;
