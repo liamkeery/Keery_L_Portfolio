d7aa05fb2ff3d7d605c5690608e52a6139820fcb
Liam Keery <l_keery@fanshaweonline.ca>
2019-12-07 17:48:38 -0500

    Port change

---
+++ config.js
@@ -0,0 +1,9 @@
+const config = {
+    host: "us-cdbr-iron-east-05.cleardb.net", //localhost
+    // port: 8889, 
+    uname: "bcdd7d72933758", // root
+    pword: "heroku_96ca2404358326c", // root
+    database: "db_portfolio"
+}
+
+module.exports = config;
