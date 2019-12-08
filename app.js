5b689afc99409dde28e1dbb65c0307098d66ff66
Liam Keery <l_keery@fanshaweonline.ca>
2019-12-08 11:38:18 -0500

    Added error handling, browser specific fixes

--- app.js
+++ app.js
@@ -23,20 +23,33 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
-});
+// 404 Error Handling
+app.use((req, res, next) => {
+  var err = new Error('Not Found');
+  err.status = 404;
+  err.customMessage = "Woops! We couldn't find that.";
 
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+  next(err);
+})
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
+app.use((err, req, res, next) => {
+  res.render('error', {data: err, layout: 'errorPage'});
+})
+
+// // catch 404 and forward to error handler
+// app.use(function(req, res, next) {
+//   next(createError(404));
+// });
+
+// // error handler
+// app.use(function(err, req, res, next) {
+//   // set locals, only providing error in development
+//   res.locals.message = err.message;
+//   res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+//   // render the error page
+//   res.status(err.status || 500);
+//   res.render('error');
+// });
 
 module.exports = app;
